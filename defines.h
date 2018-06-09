66bb3af0eb340d68693b39bff8035e08ea0d5d99
Wellington Castro <wellingtonvcastro@gmail.com>
2018-06-09 18:11:55 -0400

    primeiro commit

---
+++ defines.h
@@ -0,0 +1,20 @@
+ifndef __DEFINES__
+    #define __DEFINES__
+endif
+
+# ------------------- Timer defines ----------------------
+unsigned int mills = 0;
+unsigned int *_date;
+unsigned char *_time;
+unsigned char *_monthDays;
+# --------------------------------------------------------
+
+# ------------------- Caio defines -----------------------
+#define YEAR    2
+#define MON     1
+#define DAY     0
+
+#define HR  2
+#define MIN 1
+#define SEC 0
+# --------------------------------------------------------
---
+++ set_days.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void getDays(unsigned int year, unsigned int *yearVector)
+{
+    /* Sets 28 or 29 days for february in the yearVector according
+        to the year */
+    unsigned leapYear;
+    leapYear = 0;
+
+    if(year % 4 == 0)
+    {
+        if(year % 100 != 0) leapYear = 1;
+        else
+        {
+            if(year % 400 == 0) leapYear = 1;
+        }
+    }
+    if(leapYear) yearVector[1] = 29;
+    else yearVector[1] = 28;
+}
+
+int main(void)
+{
+    unsigned int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    unsigned int i, year;
+    while(scanf("%u", &year) != EOF)
+    {
+        getDays(year, monthDays);
+        for(i = 0; i < 12; i++)
+            printf("%d ", monthDays[i]);
+        printf("\n");
+    }
+    return 0;
+}
